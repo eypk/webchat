131d627012322683f2291c19bed7f7c5adca0c1a
eyup.kirci <eyup.kirci@finspiretech.com>
2022-10-26 15:41:30 +0200

    add: pages created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import "./App.css";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import "./styles.scss";
 
 function App() {
   return (
     <div className="App">
-      <p>Web Chat</p>
+      {/* <Register /> */}
+      <Home />
     </div>
   );
 }
